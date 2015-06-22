e3bb32949ae2eed6f1d0eb5784f0bf006185caea
Stefan Otte <stefan.otte@gmail.com>
2015-06-22 21:47:16 +0200

    Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+from setuptools import setup
+
+
+def read(fname):
+    """Utility function to read the README file."""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="nphelper",
+    description="nphelper - convenient numpy helper functions",
+    long_description=read("README.rst"),
+    author="Stefan Otte",
+    url="https://github.com/sotte/nphelper",
+    download_url="https://github.com/sotte/nphelper",
+    author_email="stefan.otte@gmail.com",
+    version="0.0.1",
+    install_requires=["numpy", ],
+    packages=["nphelper"],
+    license="MIT"
+)
